596f7a4ab3041a45021d9215a7f245260892975c
ue-sho <tssu45@gmail.com>
2021-09-07 19:18:48 +0900

    :+1: 加算以外の基本演算子を追加
    引き算、掛け算、割り算に対応

--- calculator/domain/arithmetic.py
+++ calculator/domain/arithmetic.py
@@ -7,3 +7,24 @@ class Add(BinaryOperator):
 
     def calc(self) -> int:
         return self.op1.calc() + self.op2.calc()
+
+class Substract(BinaryOperator):
+    def __init__(self, op1: Expression, op2: Expression):
+        super().__init__(op1, op2)
+
+    def calc(self) -> int:
+        return self.op1.calc() - self.op2.calc()
+
+class Multiply(BinaryOperator):
+    def __init__(self, op1: Expression, op2: Expression):
+        super().__init__(op1, op2)
+
+    def calc(self) -> int:
+        return self.op1.calc() * self.op2.calc()
+
+class Divide(BinaryOperator):
+    def __init__(self, op1: Expression, op2: Expression):
+        super().__init__(op1, op2)
+
+    def calc(self) -> int:
+        return self.op1.calc() * self.op2.calc()
