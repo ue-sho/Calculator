24f135e849c463663e20a96450ded08a5958616a
ue-sho <tssu45@gmail.com>
2021-09-11 20:45:58 +0900

    feat: requestデータを渡して計算を実行する方式に変更

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,10 @@
 import queue
-from typing import List
 
+from calculator.exception.calculate_error import CalculatorError
+from calculator.request.calculator_request import (
+    CalculatorValidRequest,
+    CalculatorCommand
+)
 from calculator.domain import (
     value,
     variable
@@ -10,54 +14,53 @@ from calculator.operator import (
     assignment
 )
 
-class CalculateUseCase:
-    PLUS = '+'
-    MINUS = '-'
-    MUL = '*'
-    DIV = '/'
-    SimpleAssign = '='
-
+class Calculator:
     def __init__(self):
         self.expression_queue = queue.LifoQueue()
         self.symbol_table = {}
 
-    def handle(self, s: List[str]) -> int:
-        for x in s:
+    def execute(self, request):
+        if request.cmd == CalculatorCommand.PRINT_CMD:
+            ex = self.expression_queue.get()
+            return ex.calc()
+        elif request.cmd == CalculatorCommand.QUIT_CMD:
+            return None
 
-            if x.isdigit():
-                ex = value.Value(int(x))
+        token_list = request.token_list
+        for token in token_list:
+            if token.isdigit():
+                ex = value.Value(int(token))
                 self.expression_queue.put(ex)
-            elif x is self.PLUS:
+            elif token == '+':
                 op2 = self.expression_queue.get()
                 op1 = self.expression_queue.get()
                 ex = arithmetic.Add(op1, op2)
                 self.expression_queue.put(ex)
-            elif x is self.MINUS:
+            elif token == '-':
                 op2 = self.expression_queue.get()
                 op1 = self.expression_queue.get()
                 ex = arithmetic.Substract(op1, op2)
                 self.expression_queue.put(ex)
-            elif x is self.MUL:
+            elif token == '*':
                 op2 = self.expression_queue.get()
                 op1 = self.expression_queue.get()
                 ex = arithmetic.Multiply(op1, op2)
                 self.expression_queue.put(ex)
-            elif x is self.DIV:
+            elif token == '/':
                 op2 = self.expression_queue.get()
                 op1 = self.expression_queue.get()
                 ex = arithmetic.Divide(op1, op2)
                 self.expression_queue.put(ex)
-            elif x is self.SimpleAssign:
+            elif token == '=':
                 op2 = self.expression_queue.get()
                 op1 = self.expression_queue.get()
                 ex = assignment.SimpleAssign(op1, op2)
                 self.expression_queue.put(ex)
-            elif x.isalpha():
-                ex = variable.Variable(x, self.symbol_table)
+            elif token.isalpha():
+                ex = variable.Variable(token, self.symbol_table)
                 self.expression_queue.put(ex)
             else:
-                raise ValueError('processエラー: %s', x)
-
+                raise CalculatorError('不正な値です: {}'.format(token))
 
         ex = self.expression_queue.get()
         return ex.calc()
--- calculator/request/calculator_request.py
+++ calculator/request/calculator_request.py
@@ -3,12 +3,17 @@ from typing import List
 from abc import ABCMeta, abstractmethod
 
 
+class CalculatorCommand:
+    QUIT_CMD = 'quit'
+    PRINT_CMD = 'print'
+    CALCULATE_CMD = 'calc'
+
 class CalculatorInvalidRequest:
     def __init__(self):
         self.errors = []
 
-    def add_error(self, parameter, message):
-        self.errors.append({"parameter": parameter, "message": message})
+    def add_error(self, value, message):
+        self.errors.append({"value": value, "message": message})
 
     def has_errors(self):
         return len(self.errors) > 0
@@ -18,9 +23,7 @@ class CalculatorInvalidRequest:
 
 
 class CalculatorValidRequest:
-
-
-    def __init__(self, cmd, token_list: [Token] = None):
+    def __init__(self, cmd, token_list: [str] = None):
         self.cmd = cmd
         self.token_list = token_list
 
@@ -29,8 +32,8 @@ class CalculatorValidRequest:
 
 
 class CalculatorRequest:
-    QUIT_CMD = ['quit', 'q', 'exit']
-    PRINT_CMD = ['print', 'p']
+    QUIT_CMD_LIST = ['quit', 'q', 'exit']
+    PRINT_CMD_LIST = ['print', 'p']
 
     @classmethod
     def create(cls, input_data: str):
@@ -38,20 +41,18 @@ class CalculatorRequest:
 
         expression_list = input_data.split()
         for ex in expression_list:
-            if ex in cls.QUIT_CMD:
-                if len(input_data) == 1:
-                    return CalculatorValidRequest('quit')
+            if ex in cls.QUIT_CMD_LIST:
+                if len(expression_list) == 1:
+                    return CalculatorValidRequest(CalculatorCommand.QUIT_CMD)
                 else:
                     invalid_req.add_error(ex, '予約語が含まれています')
-            elif ex in cls.PRINT_CMD:
-                if len(input_data) == 1:
-                    return CalculatorValidRequest('print')
+            elif ex in cls.PRINT_CMD_LIST:
+                if len(expression_list) == 1:
+                    return CalculatorValidRequest(CalculatorCommand.PRINT_CMD)
                 else:
                     invalid_req.add_error(ex, '予約語が含まれています')
-            elif int(ex) < 0:
-                invalid_req.add_error(ex, '負の値は入力できません')
 
         if invalid_req.has_errors():
             return invalid_req
 
-        return CalculatorValidRequest('calc', expression_list)
+        return CalculatorValidRequest(CalculatorCommand.CALCULATE_CMD, expression_list)
