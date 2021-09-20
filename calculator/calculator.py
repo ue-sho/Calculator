b0bdad694a9e6b5d3ff3d77c3329758457551df0
ue-sho <tssu45@gmail.com>
2021-09-20 21:53:03 +0900

    refactor: calculator.pyを修正
    関数テーブルを作成した

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -22,6 +22,40 @@ from calculator.unary_operator import (
 )
 
 class Calculator:
+    BINARY_OPERATOR_FUNCTION_TABLE = {
+       "+": lambda op1, op2: arithmetic.Add(op1, op2),
+       "-": lambda op1, op2: arithmetic.Substract(op1, op2),
+       "*": lambda op1, op2: arithmetic.Multiply(op1, op2),
+       "/": lambda op1, op2: arithmetic.Divide(op1, op2),
+       "%": lambda op1, op2: arithmetic.Modulo(op1, op2),
+       "=": lambda op1, op2: assignment.SimpleAssign(op1, op2),
+       "+=": lambda op1, op2: assignment.AddAssign(op1, op2),
+       "-=": lambda op1, op2: assignment.SubAssign(op1, op2),
+       "*=": lambda op1, op2: assignment.MulAssign(op1, op2),
+       "/=": lambda op1, op2: assignment.DivAssign(op1, op2),
+       "<<=": lambda op1, op2: assignment.LeftShiftAssign(op1, op2),
+       ">>=": lambda op1, op2: assignment.RightShiftAssign(op1, op2),
+       "%=": lambda op1, op2: assignment.ModuloAssign(op1, op2),
+       "&": lambda op1, op2: bitwise_operator.And(op1, op2),
+       "|": lambda op1, op2: bitwise_operator.Or(op1, op2),
+       "^": lambda op1, op2: bitwise_operator.Xor(op1, op2),
+       ">>": lambda op1, op2: bitwise_operator.RightShift(op1, op2),
+       "<<": lambda op1, op2: bitwise_operator.LeftShift(op1, op2),
+       "&&": lambda op1, op2: logical_operator.Both(op1, op2),
+       "||": lambda op1, op2: logical_operator.Either(op1, op2),
+       "==": lambda op1, op2: relational_operator.Equal(op1, op2),
+       "!=": lambda op1, op2: relational_operator.NotEqual(op1, op2),
+       "<": lambda op1, op2: relational_operator.Less(op1, op2),
+       "<=": lambda op1, op2: relational_operator.LessEqual(op1, op2),
+       ">": lambda op1, op2: relational_operator.Greater(op1, op2),
+       ">=": lambda op1, op2: relational_operator.GreaterEqual(op1, op2),
+    }
+    UNARY_OPERATOR_FUNCTION_TABLE = {
+       "++": lambda op1: inc_dec.Increment(op1),
+       "--": lambda op1: inc_dec.Decrement(op1),
+       "!": lambda op1: logical_not.LogicalNot(op1),
+    }
+
 
     def __init__(self):
         self.expression_queue = queue.LifoQueue()
@@ -39,150 +73,17 @@ class Calculator:
             if token.isdigit():
                 ex = value.Value(int(token))
                 self.expression_queue.put(ex)
-            elif token == '+':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = arithmetic.Add(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '-':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = arithmetic.Substract(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '*':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = arithmetic.Multiply(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '/':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = arithmetic.Divide(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '%':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = arithmetic.Modulo(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.SimpleAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '+=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.AddAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '-=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.SubAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '*=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.MulAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '/=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.DivAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '++':
-                op1 = self.expression_queue.get()
-                ex = inc_dec.Increment(op1)
-                self.expression_queue.put(ex)
-            elif token == '--':
-                op1 = self.expression_queue.get()
-                ex = inc_dec.Decrement(op1)
-                self.expression_queue.put(ex)
-            elif token == '&':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = bitwise_operator.And(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '|':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = bitwise_operator.Or(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '^':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = bitwise_operator.Xor(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '>>':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = bitwise_operator.RightShift(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '<<':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = bitwise_operator.LeftShift(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '>>=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.RightShiftAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '<<=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.LeftShiftAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '%=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = assignment.ModuloAssign(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '==':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = relational_operator.Equal(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '!=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = relational_operator.NotEqual(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '<':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = relational_operator.Less(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '<=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = relational_operator.LessEqual(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '>':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = relational_operator.Greater(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '>=':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = relational_operator.GreaterEqual(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '&&':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = logical_operator.Both(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '||':
-                op2 = self.expression_queue.get()
-                op1 = self.expression_queue.get()
-                ex = logical_operator.Either(op1, op2)
-                self.expression_queue.put(ex)
-            elif token == '!':
-                op1 = self.expression_queue.get()
-                ex = logical_not.LogicalNot(op1)
-                self.expression_queue.put(ex)
             elif token.isalpha():
                 ex = variable.Variable(token, self.symbol_table)
+                self.expression_queue.put(ex)
+            elif token in self.BINARY_OPERATOR_FUNCTION_TABLE.keys():
+                op2 = self.expression_queue.get()
+                op1 = self.expression_queue.get()
+                ex = self.BINARY_OPERATOR_FUNCTION_TABLE[token](op1, op2)
+                self.expression_queue.put(ex)
+            elif token in self.UNARY_OPERATOR_FUNCTION_TABLE.keys():
+                op1 = self.expression_queue.get()
+                ex = self.UNARY_OPERATOR_FUNCTION_TABLE[token](op1)
                 self.expression_queue.put(ex)
             else:
                 raise CalculatorError('不正な値です: {}'.format(token))
